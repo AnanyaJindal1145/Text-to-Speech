e4494a560c9e1e5e27b57c6d2f79a6024afa8b2e
AnanyaJindal1145 <ajindal1145@gmail.com>
2023-07-09 23:16:35 +0530

    added voice options

--- index.js
+++ index.js
@@ -8,12 +8,20 @@ speakButton.addEventListener("click", function() {
   let text = textArea.value;
 
   // Create a new SpeechSynthesisUtterance object
-  let utterance = new SpeechSynthesisUtterance();
+  let message = new SpeechSynthesisUtterance();
+
+  //Adding option to change voice
+  let number=0;
+  if(document.getElementById('david').checked == true){number=0;}
+  if(document.getElementById('ravi').checked == true){number=1;}
+  if(document.getElementById('heera').checked == true){number=2;}
+  if(document.getElementById('mark').checked == true){number=3;}
+  if(document.getElementById('zira').checked == true){number=4;}
 
   // Set the text and voice of the utterance
-  utterance.text = text;
-  utterance.voice = window.speechSynthesis.getVoices()[0];
+  message.text = text;
+  message.voice = window.speechSynthesis.getVoices()[number];
 
   // Speak the utterance
-  window.speechSynthesis.speak(utterance);
+  window.speechSynthesis.speak(message);
 });
