3f6e5f9293109634effec84b8bc8246a9184341c
AnanyaJindal1145 <ajindal1145@gmail.com>
2023-07-10 17:14:42 +0530

    .

--- index.js
+++ index.js
@@ -1,27 +1,16 @@
-// Get the text area and speak button elements
 let textArea = document.getElementById("text");
 let speakButton = document.getElementById("speak-button");
 
-// Add an event listener to the speak button
 speakButton.addEventListener("click", function() {
-  // Get the text from the text area
   let text = textArea.value;
-
-  // Create a new SpeechSynthesisUtterance object
-  let message = new SpeechSynthesisUtterance();
-
-  //Adding option to change voice
+  let message = new SpeechSynthesisUtterance(); //message is the new object we are making from speech synthesis class
   let number=0;
-  if(document.getElementById('david').checked == true){number=0;}
+  // if(document.getElementById('david').checked == true){number=0;}
   if(document.getElementById('ravi').checked == true){number=1;}
-  if(document.getElementById('heera').checked == true){number=2;}
-  if(document.getElementById('mark').checked == true){number=3;}
-  if(document.getElementById('zira').checked == true){number=4;}
-
-  // Set the text and voice of the utterance
-  message.text = text;
-  message.voice = window.speechSynthesis.getVoices()[number];
-
-  // Speak the utterance
+  else if(document.getElementById('heera').checked == true){number=2;}
+  else if(document.getElementById('mark').checked == true){number=3;}
+  else if(document.getElementById('zira').checked == true){number=4;}
+  message.text = text; //.text is a method
+  message.voice = window.speechSynthesis.getVoices()[number]; //.voice is a method
   window.speechSynthesis.speak(message);
 });
